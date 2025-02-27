0ca54b224921950032706293d68ed49517441207
Santiajo <ca.manzo@duocuc.cl>
2025-02-27 17:54:35 -0300

    Changes to be committed:
    modified:   resources/views/views/especialistas/fichaespecialista.blade.php
    modified:   resources/views/views/usuarios/fichausuarios.blade.php
    
    Cambios:
    - Ahora las barras de búsquedas están de pana

--- resources/views/views/especialistas/fichaespecialista.blade.php
+++ resources/views/views/especialistas/fichaespecialista.blade.php
@@ -4,111 +4,115 @@
 
 @section('content')
 
-<!-- Div para que el sidebar no moleste -->
-<div class="content">
-    <div class="fila1">
-        <h2>Listado de especialistas</h2>
-    </div>
-    <hr>
-    <div class="fila2">
-        <a class="boton-primario" id="benAgregar" href="{{ route('especialistas.formularioEspecialista') }}">
-            <p><i class='bx bx-user-plus'></i> Agregar especialista</p>
-        </a>
-        <a class="boton-primario" id="benAgregar" href="{{ route('especialistas.crudEspecialidad') }}">
-            <p><i class='bx bx-plus-medical'></i> Agregar especialidad</p>
-        </a>
-        <!-- Para buscar productos por texto -->
-        <form id='formBuscarEsp' class='barraBusqueda' method="GET" action="{{ route('especialistas.listarEspecialistas') }}">
-            <input type="text" name="benBuscar" id="benBuscar" placeholder="Buscar..."
-                value="{{ request('benBuscar') }}">
-            <button type="submit"><i class='bx bx-search'></i></button>
-            <div id='errorBarraBusqueda' class='errores'></div>
-            @error('benBuscar')
-            <div class="alert alert-danger alert2">El valor ingresado no cumple los requisitos</div>
-            @enderror
-        </form>
-    </div>
-    <div class="fila4" id="fila1Perso">
-        <form class="formularioPiola" id="formTuneado" method="GET"
-            action="{{ route('especialistas.exportarEspecialistas') }}">
-            @csrf
-            <div class="separacionFormulario">
-                <label for="fromDate">Desde:</label>
-                <input type="date" id="fromDate" name="fromDate" required>
+    <!-- Div para que el sidebar no moleste -->
+    <div class="content">
+        <div class="fila1">
+            <h2>Listado de especialistas</h2>
+        </div>
+        <hr>
+        <div class="fila2">
+            <a class="boton-primario" id="benAgregar" href="{{ route('especialistas.formularioEspecialista') }}">
+                <p><i class='bx bx-user-plus'></i> Agregar especialista</p>
+            </a>
+            <a class="boton-primario" id="benAgregar" href="{{ route('especialistas.crudEspecialidad') }}">
+                <p><i class='bx bx-plus-medical'></i> Agregar especialidad</p>
+            </a>
+            <!-- Para buscar productos por texto -->
+            <form id='formBuscarEsp' class='barraBusqueda' method="GET"
+                action="{{ route('especialistas.listarEspecialistas') }}">
+                <div class="inputBarraBusqueda">
+                    <input type="text" name="benBuscar" id="benBuscar" placeholder="Buscar..."
+                        value="{{ request('benBuscar') }}">
+                    <button type="submit"><i class='bx bx-search'></i></button>
+                </div>
 
-                <label for="toDate">Hasta:</label>
-                <input type="date" id="toDate" name="toDate" required>
-            </div>
-            <button type="submit" class="boton-secundario">
-                <i class='bx bx-export'></i> Exportar
-            </button>
-        </form>
-    </div>
-    <table>
-        <thead>
-            <tr>
-                <th>Rut</th>
-                <th>Nombre</th>
-                <th>Teléfono</th>
-                <th>Correo electrónico</th>
-                <th>Especialidad</th>
-                <th>Abreviación</th>
-                <th>Fecha de registro</th>
-                <th>Modificar</th>
-                <th>Eliminar</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($especialistas as $especialista)
+                <div id='errorBarraBusqueda' class='errores'></div>
+                @error('benBuscar')
+                    <div class="alert alert-danger alert2">El valor ingresado no cumple los requisitos</div>
+                @enderror
+            </form>
+        </div>
+        <div class="fila4" id="fila1Perso">
+            <form class="formularioPiola" id="formTuneado" method="GET"
+                action="{{ route('especialistas.exportarEspecialistas') }}">
+                @csrf
+                <div class="separacionFormulario">
+                    <label for="fromDate">Desde:</label>
+                    <input type="date" id="fromDate" name="fromDate" required>
+
+                    <label for="toDate">Hasta:</label>
+                    <input type="date" id="toDate" name="toDate" required>
+                </div>
+                <button type="submit" class="boton-secundario">
+                    <i class='bx bx-export'></i> Exportar
+                </button>
+            </form>
+        </div>
+        <table>
+            <thead>
                 <tr>
-                    <td data-label="Rut">{{ $especialista->especialistaRut }}-{{ $especialista->especialistaDv }}</td>
-                    <td data-label="Nombre">{{ $especialista->especialistaPNombre }}
-                        {{ $especialista->especialistaSNombre }}
-                    </td>
-                    <td data-label="Teléfono">{{ $especialista->especialistaTelefono }}</td>
-                    <td data-label="Correo electrónico">{{ $especialista->especialistaCorreo }}</td>
-                    @foreach ($especialidades as $especialidad)
-                        @if ($especialista->especialidad_id == $especialidad->id)
-                            <td data-label="Especialidad">{{ $especialidad->especialidadNombre }}</td>
-                            <td data-label="Abreviación">{{ $especialidad->especialidadAbreviacion }}</td>
-                        @endif
-                    @endforeach
-                    <td data-label="Fecha de registro">{{ $especialista->created_at }}</td>
-                    <td data-label="Modificar">
-                        <a class="boton-quintiario" id="benAgregar"
-                            href="{{ route('especialistas.formularioEspecialistaRelleno', $especialista->id) }}">
-                            Modificar</a>
-                    </td>
-                    <td data-label="Eliminar">
-                        <form action="{{ route('especialistas.eliminarEspecialista', $especialista->id) }}" method="POST"
-                            style="display: inline;" onsubmit="return confirmDelete(event)">
-                            @csrf
-                            @method('DELETE')
-                            <button class="boton-terciario" type="submit"><i class='bx bx-trash'></i></button>
-                        </form>
-                    </td>
-
+                    <th>Rut</th>
+                    <th>Nombre</th>
+                    <th>Teléfono</th>
+                    <th>Correo electrónico</th>
+                    <th>Especialidad</th>
+                    <th>Abreviación</th>
+                    <th>Fecha de registro</th>
+                    <th>Modificar</th>
+                    <th>Eliminar</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <br>
-    <!-- Selector para cantidad de elementos por página -->
-    <form method="GET" action="{{ route('especialistas.listarEspecialistas') }}">
-        @csrf
-        <label for="items_per_page">Resultados por página:</label>
-        <select name="items_per_page" id="items_per_page" onchange="this.form.submit()">
-            <option value="10" {{ request('items_per_page') == 10 ? 'selected' : '' }}>10</option>
-            <option value="15" {{ request('items_per_page') == 15 ? 'selected' : '' }}>15</option>
-            <option value="20" {{ request('items_per_page') == 20 ? 'selected' : '' }}>20</option>
-        </select>
-        <input type="hidden" name="benBuscar" value="{{ request('benBuscar') }}">
-    </form>
-    <br>
-    <div class="pagination">
-        {{ $especialistas->links() }}
+            </thead>
+            <tbody>
+                @foreach ($especialistas as $especialista)
+                    <tr>
+                        <td data-label="Rut">{{ $especialista->especialistaRut }}-{{ $especialista->especialistaDv }}</td>
+                        <td data-label="Nombre">{{ $especialista->especialistaPNombre }}
+                            {{ $especialista->especialistaSNombre }}
+                        </td>
+                        <td data-label="Teléfono">{{ $especialista->especialistaTelefono }}</td>
+                        <td data-label="Correo electrónico">{{ $especialista->especialistaCorreo }}</td>
+                        @foreach ($especialidades as $especialidad)
+                            @if ($especialista->especialidad_id == $especialidad->id)
+                                <td data-label="Especialidad">{{ $especialidad->especialidadNombre }}</td>
+                                <td data-label="Abreviación">{{ $especialidad->especialidadAbreviacion }}</td>
+                            @endif
+                        @endforeach
+                        <td data-label="Fecha de registro">{{ $especialista->created_at }}</td>
+                        <td data-label="Modificar">
+                            <a class="boton-quintiario" id="benAgregar"
+                                href="{{ route('especialistas.formularioEspecialistaRelleno', $especialista->id) }}">
+                                Modificar</a>
+                        </td>
+                        <td data-label="Eliminar">
+                            <form action="{{ route('especialistas.eliminarEspecialista', $especialista->id) }}" method="POST"
+                                style="display: inline;" onsubmit="return confirmDelete(event)">
+                                @csrf
+                                @method('DELETE')
+                                <button class="boton-terciario" type="submit"><i class='bx bx-trash'></i></button>
+                            </form>
+                        </td>
+
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <br>
+        <!-- Selector para cantidad de elementos por página -->
+        <form method="GET" action="{{ route('especialistas.listarEspecialistas') }}">
+            @csrf
+            <label for="items_per_page">Resultados por página:</label>
+            <select name="items_per_page" id="items_per_page" onchange="this.form.submit()">
+                <option value="10" {{ request('items_per_page') == 10 ? 'selected' : '' }}>10</option>
+                <option value="15" {{ request('items_per_page') == 15 ? 'selected' : '' }}>15</option>
+                <option value="20" {{ request('items_per_page') == 20 ? 'selected' : '' }}>20</option>
+            </select>
+            <input type="hidden" name="benBuscar" value="{{ request('benBuscar') }}">
+        </form>
+        <br>
+        <div class="pagination">
+            {{ $especialistas->links() }}
+        </div>
     </div>
-</div>
 
 
 @endsection
--- resources/views/views/usuarios/fichausuarios.blade.php
+++ resources/views/views/usuarios/fichausuarios.blade.php
@@ -4,87 +4,90 @@
 
 @section('content')
 
-<!-- Div para que el sidebar no moleste -->
-<div class="content">
-    <div class="fila1">
-        <h2>Listado de usuarios</h2>
-    </div>
-    <hr>
-    <div class="fila2">
-        <a class="boton-primario" id="benAgregar" href="{{ route('formulariousuario') }}">
-            <p><i class='bx bx-user-plus'></i> Agregar usuario</p>
-        </a>
-        <a class="boton-secundario" id="benExportar" href="{{ route('exportarUsuarios') }}"><i class='bx bx-export'></i>
-            Exportar</a>
-        <!-- Para buscar productos por texto -->
-        <form id='formBuscarUser' class='barraBusqueda' method="GET" action="{{ route('usuarios.listar') }}">
-            <input type="text" name="benBuscar" id="benBuscar" placeholder="Buscar..." value="{{ request('benBuscar') }}">
-            <button type="submit"><i class='bx bx-search'></i></button>
-            <div id='errorBarraBusqueda' class='errores'></div>
-            @error('benBuscar')
-            <div class="alert alert-danger alert2">El valor ingresado no cumple los requisitos</div>
-            @enderror
-        </form>
-    </div>
-    @if(isset($usuarios) && count($usuarios) > 0)
+    <!-- Div para que el sidebar no moleste -->
+    <div class="content">
+        <div class="fila1">
+            <h2>Listado de usuarios</h2>
+        </div>
+        <hr>
+        <div class="fila2">
+            <a class="boton-primario" id="benAgregar" href="{{ route('formulariousuario') }}">
+                <p><i class='bx bx-user-plus'></i> Agregar usuario</p>
+            </a>
+            <a class="boton-secundario" id="benExportar" href="{{ route('exportarUsuarios') }}"><i class='bx bx-export'></i>
+                Exportar</a>
+            <!-- Para buscar productos por texto -->
+            <form id='formBuscarUser' class='barraBusqueda' method="GET" action="{{ route('usuarios.listar') }}">
+                <div class="inputBarraBusqueda">
+                    <input type="text" name="benBuscar" id="benBuscar" placeholder="Buscar..."
+                        value="{{ request('benBuscar') }}">
+                    <button type="submit"><i class='bx bx-search'></i></button>
+                </div>
+                <div id='errorBarraBusqueda' class='errores'></div>
+                @error('benBuscar')
+                    <div class="alert alert-danger alert2">El valor ingresado no cumple los requisitos</div>
+                @enderror
+            </form>
+        </div>
+        @if(isset($usuarios) && count($usuarios) > 0)
             <p>Usuarios cargados correctamente.</p>
         @else
             <p>No hay usuarios registrados.</p>
         @endif
-    <table>
-        <thead>
-            <tr>
-                <th>Rut</th>
-                <th>Nombre</th>
-                <th>Teléfono</th>
-                <th>Correo electrónico</th>
-                <th>Fecha de registro</th>
-                <th>Detalles</th>
-                <th>Modificar</th>
-                <th>Eliminar</th>
-            </tr>
-        </thead>
+        <table>
+            <thead>
+                <tr>
+                    <th>Rut</th>
+                    <th>Nombre</th>
+                    <th>Teléfono</th>
+                    <th>Correo electrónico</th>
+                    <th>Fecha de registro</th>
+                    <th>Detalles</th>
+                    <th>Modificar</th>
+                    <th>Eliminar</th>
+                </tr>
+            </thead>
 
-        <tbody>
-    @if(isset($usuarios) && count($usuarios) > 0)
-        @foreach ($usuarios as $usuario)
-            <tr>
-                <td data-label="Rut">{{ $usuario->rut }}-{{ $usuario->dv }}</td>
-                <td data-label="Nombre">
-                    {{ $usuario->primer_nombre }} 
-                    {{ $usuario->segundo_nombre ? $usuario->segundo_nombre : '' }} 
-                    {{ $usuario->apellido_paterno }} 
-                    {{ $usuario->apellido_materno }}
-                </td>
-                <td data-label="Teléfono">{{ $usuario->telefono }}</td>
-                <td data-label="Correo electrónico">{{ $usuario->email }}</td>
-                <td data-label="Fecha de registro">{{ $usuario->created_at->format('d/m/Y') }}</td>
-                <td data-label="Acciones">
-                    <a class="detalles" href="{{ route('vistaUsuario', $usuario->id) }}">
-                        <i class='bx bxs-file-doc'></i>
-                    </a>
-                </td>
-                <td data-label="Modificar">
-                    <a class="boton-quintiario" href="{{ route('usuarios.edit', $usuario->id) }}">Modificar</a>
-                </td>
-                <td data-label="Eliminar">
-                    <form method="POST" action="{{ route('usuarios.destroy', $usuario->id) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="boton-terciario">
-                            <i class='bx bx-trash'></i> Eliminar
-                        </button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-        @else
-            <tr>
-                <td colspan="8">No hay usuarios registrados.</td>
-            </tr>
-        @endif
+            <tbody>
+                @if(isset($usuarios) && count($usuarios) > 0)
+                    @foreach ($usuarios as $usuario)
+                        <tr>
+                            <td data-label="Rut">{{ $usuario->rut }}-{{ $usuario->dv }}</td>
+                            <td data-label="Nombre">
+                                {{ $usuario->primer_nombre }}
+                                {{ $usuario->segundo_nombre ? $usuario->segundo_nombre : '' }}
+                                {{ $usuario->apellido_paterno }}
+                                {{ $usuario->apellido_materno }}
+                            </td>
+                            <td data-label="Teléfono">{{ $usuario->telefono }}</td>
+                            <td data-label="Correo electrónico">{{ $usuario->email }}</td>
+                            <td data-label="Fecha de registro">{{ $usuario->created_at->format('d/m/Y') }}</td>
+                            <td data-label="Acciones">
+                                <a class="detalles" href="{{ route('vistaUsuario', $usuario->id) }}">
+                                    <i class='bx bxs-file-doc'></i>
+                                </a>
+                            </td>
+                            <td data-label="Modificar">
+                                <a class="boton-quintiario" href="{{ route('usuarios.edit', $usuario->id) }}">Modificar</a>
+                            </td>
+                            <td data-label="Eliminar">
+                                <form method="POST" action="{{ route('usuarios.destroy', $usuario->id) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="boton-terciario">
+                                        <i class='bx bx-trash'></i> Eliminar
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <td colspan="8">No hay usuarios registrados.</td>
+                    </tr>
+                @endif
             </tbody>
-     </table>
+        </table>
         <br>
         <!-- Selector para cantidad de elementos por página -->
         <form method="GET" action="{{ route('usuarios.listar') }}">
@@ -102,5 +105,5 @@
         <div class="pagination">
             {{ $usuarios->links() }}
         </div>
-</div>
+    </div>
 @endsection
